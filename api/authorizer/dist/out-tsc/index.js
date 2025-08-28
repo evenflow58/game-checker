c9afcf827a385c7986f52db9a8803241cbc75bdc
evenflow58 <evenflow58@gmail.com>
2025-08-28 09:03:08 -0400

    Fix some build issues.

--- api/authorizer/dist/out-tsc/index.js
+++ api/authorizer/dist/out-tsc/index.js
@@ -1,58 +1,43 @@
-import { APIGatewayAuthorizerResult, APIGatewayRequestAuthorizerEventV2, StatementEffect } from "aws-lambda";
-import { createRemoteJWKSet, jwtVerify, JWTPayload } from "jose";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const jose_1 = require("jose");
 const GOOGLE_ISSUER = "https://accounts.google.com";
 const GOOGLE_JWKS_URI = "https://www.googleapis.com/oauth2/v3/certs";
-
 const CLIENT_ID = process.env['GOOGLE_CLIENT_ID'] ?? "";
-
-export const handler = async (
-    event: APIGatewayRequestAuthorizerEventV2
-): Promise<APIGatewayAuthorizerResult> => {
+const handler = async (event) => {
     console.log("Received event:", JSON.stringify(event, null, 2));
-
     try {
         const token = (event.headers || {})['Auth'];
-
-        if (!token) throw new Error("No token present");
-
+        if (!token)
+            throw new Error("No token present");
         const { payload } = await verifyGoogleToken(token);
-
         // Use Google "sub" (unique user ID) as principalId
         const principalId = payload?.sub;
-
-        if (!principalId) throw new Error("No google identity returned. Could not get anything from google.")
-
-        return generateAllow(
-            principalId,
-            event.routeArn,
-            {
-                email: payload['email'] as string ?? "",
-            });
-    } catch (err) {
+        if (!principalId)
+            throw new Error("No google identity returned. Could not get anything from google.");
+        return generateAllow(principalId, event.routeArn, {
+            email: payload['email'] ?? "",
+        });
+    }
+    catch (err) {
         console.error("❌ Google auth failed:", err);
         return generateDeny("unauthorized", event.routeArn);
     }
 };
-
+exports.handler = handler;
 /**
  * Verify Google JWT using jose
  */
-async function verifyGoogleToken(token: string): Promise<{ payload: JWTPayload }> {
-    const jwks = createRemoteJWKSet(new URL(GOOGLE_JWKS_URI));
-    return jwtVerify(token, jwks, {
+async function verifyGoogleToken(token) {
+    const jwks = (0, jose_1.createRemoteJWKSet)(new URL(GOOGLE_JWKS_URI));
+    return (0, jose_1.jwtVerify)(token, jwks, {
         issuer: GOOGLE_ISSUER,
         audience: CLIENT_ID,
     });
 }
-
 // Help function to generate an IAM policy
-function generatePolicy(
-    principalId: string,
-    effect: StatementEffect,
-    resource: string,
-    context: Record<string, string> = {}
-): APIGatewayAuthorizerResult {
+function generatePolicy(principalId, effect, resource, context = {}) {
     return {
         principalId,
         policyDocument: {
@@ -68,11 +53,10 @@ function generatePolicy(
         context,
     };
 }
-
-var generateAllow = function (principalId: string, resource: string, context: Record<string, string>) {
+var generateAllow = function (principalId, resource, context) {
     return generatePolicy(principalId, "Allow", resource, context);
-}
-
-var generateDeny = function (principalId: string, resource: string) {
+};
+var generateDeny = function (principalId, resource) {
     return generatePolicy(principalId, "Deny", resource);
-}
+};
+//# sourceMappingURL=index.js.map
