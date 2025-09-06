be67600c52f2f745c09f1ed153b1032e2d26095b
evenflow58 <evenflow58@gmail.com>
2025-09-06 12:29:19 -0400

    Add dynamodb (#9)
    * Deploy database
    
    * Fix location
    
    * Send all data to the S3 bucket
    
    * Figure out where we are
    
    * Better checking
    
    * Clean up location checking
    
    * More location checking
    
    * More searching
    
    * Trying ChatGPT response
    
    * Add base data output
    
    * Doing step 1
    
    * Fix pipeline
    
    * Trying extra build artifact
    
    * Upload all files
    
    * Pushing change to trigger build
    
    * Just return all files
    
    * Get children folders
    
    * Get the secondary artifact
    
    * Only one artifact
    
    * Add table call to test endpoint
    
    * Add dynamo rights
    
    * Fix reference
    
    * Change table column and fix table call
    
    * Make the data column not part of the key
    
    * Keep token on refresh

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     GOOGLE_CLIENT_ID: "526294520699-n3mfhhfmf1ucqhmbgj1kqir4i5f9fb2j.apps.googleusercontent.com",
-    apiUrl: "https://api-retreive-google-token.doiownthatgame.com",
+    apiUrl: "https://api-add-dynamodb.doiownthatgame.com",
 };
