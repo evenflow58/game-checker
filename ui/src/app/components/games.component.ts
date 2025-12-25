0e4c46f9aa891fe946a19128585b0567620794d5
evenflow58 <evenflow58@gmail.com>
2025-12-25 12:26:58 -0500

    Use img_icon_url for game card images

--- ui/src/app/components/games.component.ts
+++ ui/src/app/components/games.component.ts
@@ -43,12 +43,12 @@ import { GamesService, SteamGame } from '../services/games.service';
             <div *ngFor="let game of games()" class="game-card">
               <div class="game-image">
                 <img 
-                  *ngIf="game.img_logo_url" 
+                  *ngIf="game.img_icon_url" 
                   [src]="getGameImageUrl(game)" 
                   [alt]="game.name"
                   (error)="onImageError($event)"
                 >
-                <div *ngIf="!game.img_logo_url" class="game-placeholder">🎮</div>
+                <div *ngIf="!game.img_icon_url" class="game-placeholder">🎮</div>
               </div>
               <div class="game-info">
                 <h3 class="game-name">{{ game.name }}</h3>
@@ -439,8 +439,8 @@ export class GamesComponent implements OnInit {
   }
 
   getGameImageUrl(game: SteamGame): string {
-    if (game.img_logo_url) {
-      return `https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_logo_url}.jpg`;
+    if (game.img_icon_url) {
+      return `https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`;
     }
     return '';
   }
